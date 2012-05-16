1ba60130de57af7a914421725b2b1756cf23e4c9
anisart <me@anisart.ru>
2012-05-16 23:11:40 +0400

    multiple oscillators. dont working

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -7,46 +7,47 @@ MainWindow::MainWindow(QWidget *parent) :
 {
     ui->setupUi(this);
 
-    curve1 = new QwtPlotCurve("Curve 1");
-    curve1->setRenderHint(QwtPlotItem::RenderAntialiased);
-    curve2 = new QwtPlotCurve("Curve 2");
-    curve2->setRenderHint(QwtPlotItem::RenderAntialiased);
-    curve3 = new QwtPlotCurve("Curve 3");
-    curve3->setRenderHint(QwtPlotItem::RenderAntialiased);
+    num = 2;
+    curve = new QwtPlotCurve * [num];
+    mark = new QwtPlotMarker * [num];
 
-    curve4 = new QwtPlotCurve("Curve 4");
-    curve4->setRenderHint(QwtPlotItem::RenderAntialiased);
-    curve4->setPen(QPen(Qt::blue, 1));
-    curve5 = new QwtPlotCurve("Curve 5");
-    curve5->setRenderHint(QwtPlotItem::RenderAntialiased);
-    curve5->setPen(QPen(Qt::red,1));
+    for (int i = 0; i < num; ++i)
+    {
+        curve[i] = new QwtPlotCurve("Curve "/* + QString(i)*/);
+        curve[i]->setRenderHint(QwtPlotItem::RenderAntialiased);
+        curve[i]->setPen(QPen(Qt::red,1));
 
-    mark1 = new QwtPlotMarker;
-    mark1->setSymbol(new QwtSymbol(QwtSymbol::Ellipse, QColor(Qt::transparent), QColor(Qt::blue), QSize(5,5)));
-    mark2 = new QwtPlotMarker;
-    mark2->setSymbol(new QwtSymbol(QwtSymbol::Ellipse, QColor(Qt::transparent), QColor(Qt::red), QSize(5,5)));
+        mark[i] = new QwtPlotMarker;
+        mark[i]->setSymbol(new QwtSymbol(QwtSymbol::Ellipse, QColor(Qt::transparent), QColor(Qt::red), QSize(5,5)));
+    }
 
-    ui->qwtPlot->setAxisScale(QwtPlot::xBottom, -20, 20);
-    ui->qwtPlot->setAxisScale(QwtPlot::yLeft, -20, 20);
-    ui->qwtPlot_2->setAxisScale(QwtPlot::xBottom, -20, 20);
-    ui->qwtPlot_2->setAxisScale(QwtPlot::yLeft, -20, 20);
-    ui->qwtPlot_4->setAxisScale(QwtPlot::xBottom, -20, 20);
-    ui->qwtPlot_4->setAxisScale(QwtPlot::yLeft, -20, 20);
+    //    ui->qwtPlot->setAxisScale(QwtPlot::xBottom, -20, 20);
+    //    ui->qwtPlot->setAxisScale(QwtPlot::yLeft, -20, 20);
 
     solved = false;
-    w1 = 0.98;
-    w2 = 1.02;
     d = 0.5;
     a = 0.16;
     r = 2;
     Tmin = 0;
     Tmax = 200;
-    stepCount = 10000;
-    j = 0;
+    count = 10000;
+    t = 0;
 
-    solver(5, 5, 5, 1, 1, 1);
-    paintGraphs();
-    startTimer(1);
+    w = new double[num]; // !!!
+    w[0] = 0.98;
+    w[1] = 1.02;
+
+    double startX[] = {5, 1};
+    double startY[] = {5, 1};
+    double startZ[] = {5, 1};
+
+    solver(startX, startY, startZ);
+    //    paintGraphs();
+    startTimer(1000);
+    //    QwtPlotCurve *cur = new QwtPlotCurve;
+    //    curve[0]->setSamples(x[0], y[0], count);
+    //    curve[0]->attach(ui->qwtPlot);
+    //    ui->qwtPlot->replot();
 }
 
 MainWindow::~MainWindow()
@@ -54,188 +55,170 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
-void MainWindow::paintGraphs()
+//void MainWindow::paintGraphs()
+//{
+//    if ((!solved))
+//        return;
+
+//    curve1->setRawSamples(x1, y1, count);
+//    curve1->attach(ui->qwtPlot);
+//    ui->qwtPlot->replot();
+
+//    curve2->setRawSamples(x2, y2, count);
+//    curve2->attach(ui->qwtPlot_2);
+//    ui->qwtPlot_2->replot();
+
+//    double  *i_Tmax = new double [count];
+//    for (int i = 0; i < count; ++i)
+//        i_Tmax[i] = i * Tmax / count;
+//    curve3->setRawSamples(i_Tmax, u1, count);
+//    curve3->attach(ui->qwtPlot_3);
+//    ui->qwtPlot_3->replot();
+
+//}
+
+void MainWindow::solver(double *x0, double *y0, double *z0)
 {
-    if ((!solved))
-        return;
+    x = new double * [num];
+    y = new double * [num];
+    z = new double * [num];
 
-    curve1->setRawSamples(x1, y1, stepCount);
-    curve1->attach(ui->qwtPlot);
-    ui->qwtPlot->replot();
+    //    dxdt = new double * [num];
+    //    dydt = new double * [num];
+    //    dzdt = new double * [num];
 
-    curve2->setRawSamples(x2, y2, stepCount);
-    curve2->attach(ui->qwtPlot_2);
-    ui->qwtPlot_2->replot();
+    //    dxdt2 = new double * [num];
+    //    dydt2 = new double * [num];
 
-    double  *i_Tmax = new double [stepCount];
-    for (int i = 0; i < stepCount; ++i)
-        i_Tmax[i] = i * Tmax / stepCount;
-    curve3->setRawSamples(i_Tmax, u1, stepCount);
-    curve3->attach(ui->qwtPlot_3);
-    ui->qwtPlot_3->replot();
+    //    u = new double * [num];
 
-}
+    //    double X0[num], Y0[num], Z0[num];
 
-void MainWindow::solver(double x10, double y10, double z10, double x20, double y20, double z20)
-{
-    x1 = new double[stepCount];
-    y1 = new double[stepCount];
-    z1 = new double[stepCount];
-    x2 = new double[stepCount];
-    y2 = new double[stepCount];
-    z2 = new double[stepCount];
-    dx1dt = new double[stepCount];
-    dy1dt = new double[stepCount];
-    dz1dt = new double[stepCount];
-    dx2dt = new double[stepCount];
-    dy2dt = new double[stepCount];
-    dz2dt = new double[stepCount];
-
-    dx1dt2 = new double[stepCount];
-    dy1dt2 = new double[stepCount];
-    dx2dt2 = new double[stepCount];
-    dy2dt2 = new double[stepCount];
-
-    u1 = new double[stepCount];
-    u2 = new double[stepCount];
-
-    x1[0] = x10; y1[0] = y10; z1[0] = z10; u1[0] = 0;
-    x2[0] = x20; y2[0] = y20; z2[0] = z20; u2[0] = 0;
-    dx1dt[0] = dx1(x1[0], y1[0], z1[0], x2[0], y2[0], z2[0]);
-    dy1dt[0] = dy1(x1[0], y1[0], z1[0], x2[0], y2[0], z2[0]);
-    dz1dt[0] = dz1(x1[0], y1[0], z1[0], x2[0], y2[0], z2[0]);
-    dx2dt[0] = dx2(x2[0], y2[0], z2[0], x2[0], y2[0], z2[0]);
-    dy2dt[0] = dy2(x2[0], y2[0], z2[0], x2[0], y2[0], z2[0]);
-    dz2dt[0] = dz2(x2[0], y2[0], z2[0], x2[0], y2[0], z2[0]);
-
-    double dt = (Tmax - Tmin) / stepCount;
-    int k = 0;
-
-    for (int i = 1; i < stepCount; i++)
+    for (int j = 0; j < num; ++j)
     {
-        double X1, X2, X3, X4, Y1, Y2, Y3, Y4, Z1, Z2, Z3, Z4;
-        double X12, X22, X32, X42, Y12, Y22, Y32, Y42, Z12, Z22, Z32, Z42;
+        x[j] = new double[count];
+        y[j] = new double[count];
+        z[j] = new double[count];
 
-        X1 = dx1(x1[i - 1], y1[i - 1], z1[i - 1], x2[i - 1], y2[i - 1], z2[i - 1]) * dt;
-        Y1 = dy1(x1[i - 1], y1[i - 1], z1[i - 1], x2[i - 1], y2[i - 1], z2[i - 1]) * dt;
-        Z1 = dz1(x1[i - 1], y1[i - 1], z1[i - 1], x2[i - 1], y2[i - 1], z2[i - 1]) * dt;
+        //        dxdt[j] = new double[count];
+        //        dydt[j] = new double[count];
+        //        dzdt[j] = new double[count];
 
-        X12 = dx2(x1[i - 1], y1[i - 1], z1[i - 1], x2[i - 1], y2[i - 1], z2[i - 1]) * dt;
-        Y12 = dy2(x1[i - 1], y1[i - 1], z1[i - 1], x2[i - 1], y2[i - 1], z2[i - 1]) * dt;
-        Z12 = dz2(x1[i - 1], y1[i - 1], z1[i - 1], x2[i - 1], y2[i - 1], z2[i - 1]) * dt;
+        //        dxdt2[j] = new double[count];
+        //        dydt2[j] = new double[count];
 
-        X2 = dx1(x1[i - 1] + X1 / 2, y1[i - 1] + Y1 / 2, z1[i - 1] + Z1 / 2,
-                 x2[i - 1] + X12 / 2, y2[i - 1] + Y12 / 2, z2[i - 1] + Z12 / 2) * dt;
-        Y2 = dy1(x1[i - 1] + X1 / 2, y1[i - 1] + Y1 / 2, z1[i - 1] + Z1 / 2,
-                 x2[i - 1] + X12 / 2, y2[i - 1] + Y12 / 2, z2[i - 1] + Z12 / 2) * dt;
-        Z2 = dz1(x1[i - 1] + X1 / 2, y1[i - 1] + Y1 / 2, z1[i - 1] + Z1 / 2,
-                 x2[i - 1] + X12 / 2, y2[i - 1] + Y12 / 2, z2[i - 1] + Z12 / 2) * dt;
+        //        u[j] = new double[count];
 
-        X22 = dx2(x1[i - 1] + X1 / 2, y1[i - 1] + Y1 / 2, z1[i - 1] + Z1 / 2,
-                  x2[i - 1] + X12 / 2, y2[i - 1] + Y12 / 2, z2[i - 1] + Z12 / 2) * dt;
-        Y22 = dy2(x1[i - 1] + X1 / 2, y1[i - 1] + Y1 / 2, z1[i - 1] + Z1 / 2,
-                  x2[i - 1] + X12 / 2, y2[i - 1] + Y12 / 2, z2[i - 1] + Z12 / 2) * dt;
-        Z22 = dz2(x1[i - 1] + X1 / 2, y1[i - 1] + Y1 / 2, z1[i - 1] + Z1 / 2,
-                  x2[i - 1] + X12 / 2, y2[i - 1] + Y12 / 2, z2[i - 1] + Z12 / 2) * dt;
+        x[j][0] = x0[j]; y[j][0] = y0[j]; z[j][0] = z0[j];
+        //        u[j][0] = 0;
+        //        X0[i] = x0[i]; Y0[i] = y0[i]; Z0 [i] = z0[i];
+    }
+    //    for (int i = 0; i < num; ++i)
+    //    {
+    //        dxdt[i][0] = dx(X0, Y0, Z0, i, 0);
+    //        dydt[i][0] = dy(X0, Y0, Z0, i, 0);
+    //        dzdt[i][0] = dz(X0, Y0, Z0, i, 0);
+    //    }
 
-        X3 = dx1(x1[i - 1] + X2 / 2, y1[i - 1] + Y2 / 2, z1[i - 1] + Z2 / 2,
-                 x2[i - 1] + X22 / 2, y2[i - 1] + Y22 / 2, z2[i - 1] + Z22 / 2) * dt;
-        Y3 = dy1(x1[i - 1] + X2 / 2, y1[i - 1] + Y2 / 2, z1[i - 1] + Z2 / 2,
-                 x2[i - 1] + X22 / 2, y2[i - 1] + Y22 / 2, z2[i - 1] + Z22 / 2) * dt;
-        Z3 = dz1(x1[i - 1] + X2 / 2, y1[i - 1] + Y2 / 2, z1[i - 1] + Z2 / 2,
-                 x2[i - 1] + X22 / 2, y2[i - 1] + Y22 / 2, z2[i - 1] + Z22 / 2) * dt;
+    double dt = (Tmax - Tmin) / count;
 
-        X32 = dx2(x1[i - 1] + X2 / 2, y1[i - 1] + Y2 / 2, z1[i - 1] + Z2 / 2,
-                  x2[i - 1] + X22 / 2, y2[i - 1] + Y22 / 2, z2[i - 1] + Z22 / 2) * dt;
-        Y32 = dy2(x1[i - 1] + X2 / 2, y1[i - 1] + Y2 / 2, z1[i - 1] + Z2 / 2,
-                  x2[i - 1] + X22 / 2, y2[i - 1] + Y22 / 2, z2[i - 1] + Z22 / 2) * dt;
-        Z32 = dz2(x1[i - 1] + X2 / 2, y1[i - 1] + Y2 / 2, z1[i - 1] + Z2 / 2,
-                  x2[i - 1] + X22 / 2, y2[i - 1] + Y22 / 2, z2[i - 1] + Z22 / 2) * dt;
+    for (int i = 1; i < count; i++)
+    {
+        double X1[num], X2[num], X3[num], X4[num];
+        double Y1[num], Y2[num], Y3[num], Y4[num];
+        double Z1[num], Z2[num], Z3[num], Z4[num];
 
-        X4 = dx1(x1[i - 1] + X3, y1[i - 1] + Y3, z1[i - 1] + Z3,
-                 x2[i - 1] + X32, y2[i - 1] + Y32, z2[i - 1] + Z32) * dt;
-        Y4 = dy1(x1[i - 1] + X3, y1[i - 1] + Y3, z1[i - 1] + Z32,
-                 x2[i - 1] + X32, y2[i - 1] + Y32, z2[i - 1] + Z32) * dt;
-        Z4 = dz1(x1[i - 1] + X3, y1[i - 1] + Y32, z1[i - 1] + Z32,
-                 x2[i - 1] + X32, y2[i - 1] + Y32, z2[i - 1] + Z32) * dt;
-
-        X42 = dx2(x1[i - 1] + X3, y1[i - 1] + Y3, z1[i - 1] + Z3,
-                  x2[i - 1] + X32, y2[i - 1] + Y32, z2[i - 1] + Z32) * dt;
-        Y42 = dy2(x1[i - 1] + X3, y1[i - 1] + Y3, z1[i - 1] + Z32,
-                  x2[i - 1] + X32, y2[i - 1] + Y32, z2[i - 1] + Z32) * dt;
-        Z42 = dz2(x1[i - 1] + X3, y1[i - 1] + Y32, z1[i - 1] + Z32,
-                  x2[i - 1] + X32, y2[i - 1] + Y32, z2[i - 1] + Z32) * dt;
-
-
-        x1[i] = x1[i - 1] + (X1 + 2 * X2 + 2 * X3 + X4) / 6;
-        y1[i] = y1[i - 1] + (Y1 + 2 * Y2 + 2 * Y3 + Y4) / 6;
-        z1[i] = z1[i - 1] + (Z1 + 2 * Z2 + 2 * Z3 + Z4) / 6;
-
-        x2[i] = x2[i - 1] + (X12 + 2 * X22 + 2 * X32 + X42) / 6;
-        y2[i] = y2[i - 1] + (Y12 + 2 * Y22 + 2 * Y32 + Y42) / 6;
-        z2[i] = z2[i - 1] + (Z12 + 2 * Z22 + 2 * Z32 + Z42) / 6;
-
-        dx1dt[i] = dx1(x1[i], y1[i], z1[i], x2[i], y2[i], z2[i]);
-        dy1dt[i] = dy1(x1[i], y1[i], z1[i], x2[i], y2[i], z2[i]);
-        dz1dt[i] = dz1(x1[i], y1[i], z1[i], x2[i], y2[i], z2[i]);
-
-        dx2dt[i] = dx2(x1[i], y1[i], z1[i], x2[i], y2[i], z2[i]);
-        dy2dt[i] = dy2(x1[i], y1[i], z1[i], x2[i], y2[i], z2[i]);
-        dz2dt[i] = dz2(x1[i], y1[i], z1[i], x2[i], y2[i], z2[i]);
-
-        if (i < stepCount && i > 1)
+        double x_[num], y_[num], z_[num];
+        for (int j = 0; j < num; ++j)
         {
-            dx1dt2[i] = (4 * dx1dt[i - 1] - 3 * dx1dt[i - 2] - dx1dt[i]) / (2 * dt);
-            dx2dt2[i] = (4 * dx2dt[i - 1] - 3 * dx2dt[i - 2] - dx2dt[i]) / (2 * dt);
-            dy1dt2[i] = (4 * dy1dt[i - 1] - 3 * dy1dt[i - 2] - dy1dt[i]) / (2 * dt);
-            dy2dt2[i] = (4 * dy2dt[i - 1] - 3 * dy2dt[i - 2] - dy2dt[i]) / (2 * dt);
+            x_[j] = x[j][i-1];
+            y_[j] = y[j][i-1];
+            z_[j] = z[j][i-1];
         }
+
+        for (int j = 0; j < num; ++j)
+        {
+            X1[j] = x[j][i-1] + dx(x_, y_, z_, j, i) * dt / 2;
+            Y1[j] = y[j][i-1] + dy(x_, y_, z_, j, i) * dt / 2;
+            Z1[j] = z[j][i-1] + dz(x_, y_, z_, j, i) * dt / 2;
+        }
+
+        for (int j = 0; j < num; ++j)
+        {
+            X2[j] = x[j][i-1] + dx(X1, Y1, Z1, j, i) * dt / 2;
+            Y2[j] = y[j][i-1] + dy(X1, Y1, Z1, j, i) * dt / 2;
+            Z2[j] = z[j][i-1] + dz(X1, Y1, Z1, j, i) * dt / 2;
+        }
+
+        for (int j = 0; j < num; ++j)
+        {
+            X3[j] = x[j][i-1] + dx(X2, Y2, Z2, j, i) * dt;
+            Y3[j] = y[j][i-1] + dy(X2, Y2, Z2, j, i) * dt;
+            Z3[j] = z[j][i-1] + dz(X2, Y2, Z2, j, i) * dt;
+        }
+
+        for (int j = 0; j < num; ++j)
+        {
+            X4[j] = dx(X3, Y3, Z3, j, i) * dt;
+            Y4[j] = dy(X3, Y3, Z3, j, i) * dt;
+            Z4[j] = dz(X3, Y3, Z3, j, i) * dt;
+
+            x[j][i] = x[j][i-1] + (X1[j] + 2 * X2[j] + 2 * X3[j] + X4[j]) / 6;
+            y[j][i] = y[j][i-1] + (Y1[j] + 2 * Y2[j] + 2 * Y3[j] + Y4[j]) / 6;
+            z[j][i] = z[j][i-1] + (Z1[j] + 2 * Z2[j] + 2 * Z3[j] + Z4[j]) / 6;
+        }
+
+
+
+        //        dx1dt[i] = dx1(x1[i], y1[i], z1[i], x2[i], y2[i], z2[i]);
+        //        dy1dt[i] = dy1(x1[i], y1[i], z1[i], x2[i], y2[i], z2[i]);
+        //        dz1dt[i] = dz1(x1[i], y1[i], z1[i], x2[i], y2[i], z2[i]);
+
+        //        if (i < count && i > 1)
+        //        {
+        //            dx1dt2[i] = (4 * dx1dt[i - 1] - 3 * dx1dt[i - 2] - dx1dt[i]) / (2 * dt);
+        //            dx2dt2[i] = (4 * dx2dt[i - 1] - 3 * dx2dt[i - 2] - dx2dt[i]) / (2 * dt);
+        //            dy1dt2[i] = (4 * dy1dt[i - 1] - 3 * dy1dt[i - 2] - dy1dt[i]) / (2 * dt);
+        //            dy2dt2[i] = (4 * dy2dt[i - 1] - 3 * dy2dt[i - 2] - dy2dt[i]) / (2 * dt);
+        //        }
     }
 
     solved = true;
-    double sum1 = 0, sum2 = 0;
-    for (int i = 2; i < stepCount; i++)
-    {
-        u1[i] = -(dy1dt[i] * dx1dt2[i] - dx1dt[i] * dy1dt2[i]) / (dy1dt[i] * dy1dt[i] + dx1dt[i] * dx1dt[i]);
-        u2[i] = -(dy2dt[i] * dx2dt2[i] - dx2dt[i] * dy2dt2[i]) / (dy2dt[i] * dy2dt[i] + dx2dt[i] * dx2dt[i]);
-        sum1 += u1[i];
-        sum2 += u2[i];
-    }
+    //    double sum1 = 0;
+    //    for (int i = 2; i < count; i++)
+    //    {
+    //        u1[i] = -(dy1dt[i] * dx1dt2[i] - dx1dt[i] * dy1dt2[i]) / (dy1dt[i] * dy1dt[i] + dx1dt[i] * dx1dt[i]);
+    //        sum1 += u1[i];
+    //        sum2 += u2[i];
+    //    }
 
-    sum1 /= stepCount - 2;
-    sum2 /= stepCount - 2;
+    //    sum1 /= count - 2;
 }
 
 void MainWindow::timerEvent(QTimerEvent *e)
 {
     if (!solved)
         return;
-    j++;
+    t++;
 
-    QVector <double> x1r, y1r, x2r, y2r;
-    int ind = std::max(0, j - 50000 / (int)Tmax);
-    for (int l = ind; l < j; l++)
+    for (int j = 0; j < num; ++j)
     {
-        x1r.append(x1[l]);
-        y1r.append(y1[l]);
-        x2r.append(x2[l]);
-        y2r.append(y2[l]);
+        QVector <double> xr, yr;
+        int ind = std::max(0, t - 50000 / (int)Tmax);
+        for (int l = ind; l < t; l++)
+        {
+            xr.append(x[j][l]);
+            yr.append(y[j][l]);
+        }
+
+        curve[j]->setSamples(xr, yr);
+        curve[j]->attach(ui->qwtPlot);
+        mark[j]->setValue(x[j][t], y[j][t]);
+        mark[j]->attach(ui->qwtPlot);
     }
-//    qDebug()<<j;
 
-    curve4->setSamples(x1r, y1r);
-    curve4->attach(ui->qwtPlot_4);
-    curve5->setSamples(x2r, y2r);
-    curve5->attach(ui->qwtPlot_4);
-
-    mark1->setValue(x1[j],y1[j]);
-    mark1->attach(ui->qwtPlot_4);
-    mark2->setValue(x2[j],y2[j]);
-    mark2->attach(ui->qwtPlot_4);
-
-    ui->qwtPlot_4->replot();
-
-    if (j > stepCount - 3)
+    ui->qwtPlot->replot();
+    if (t > count - 3)
     {
         killTimer(e->timerId());
         return;
--- mainwindow.h
+++ mainwindow.h
@@ -18,70 +18,54 @@ class MainWindow : public QMainWindow
 public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
+
+//    enum { num = 2 };
     
 private:
     Ui::MainWindow *ui;
-    QwtPlotCurve *curve1, *curve2, *curve3, *curve4, *curve5;
-    QwtPlotMarker* mark1, *mark2;
+    QwtPlotCurve **curve; //*curve [num];
+    QwtPlotMarker **mark;
 
-    int j;
-    double *x1, *y1, *z1;
-    double *x2, *y2, *z2;
-    double *dx1dt, *dy1dt, *dz1dt;
-    double *dx2dt, *dy2dt, *dz2dt;
-    double *dx1dt2, *dy1dt2, *dz1dt2;
-    double *dx2dt2, *dy2dt2, *dz2dt2;
-    double *u1;  //частота
-    double *u2;
+    int t;
+    double **x, **y, **z;
+    double **dxdt, **dydt, **dzdt;
+    double **dxdt2, **dydt2, **dzdt2;
+    double **u;  //частота
 
-    double w1;
-    double w2;
+    double *w;
     double d;
     double a;
     double r;
     double Tmin;
     double Tmax;
-    int stepCount;
+    int count;
+    int num;
 
     bool solved;
-    void solver(double x10, double y10, double z10, double x20, double y20, double z20);
+    void solver(double *x0, double *y0, double *z0);
 
-    double dx1(double x1, double y1, double z1, double x2, double y2, double z2)
+    double dx(double *X, double *Y, double *Z, int k, int l)
     {
-        return -w1*y1 - z1;
+        return -w[k] * Y[k] - Z[k];
     }
 
 
-    double dy1(double x1, double y1, double z1, double x2, double y2, double z2)
+    double dy(double *X, double *Y, double *Z, int k, int l)
     {
-        if ((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2) < r * r)
-            return w1 * x1 + a * y1 + d * (y2 - y1);
-        else return w1 * x1 + a * y1;
+        double sync = 0;
+        for (int i = 0; i < num; ++i)
+            if ((x[i][l] - x[k][l]) * (x[i][l] - x[k][l]) + (y[i][l] - y[k][l]) * (y[i][l] - y[k][l]) < r * r)
+                sync += d * (Y[i] - Y[k]);
+
+        return w[k] * X[k] + a * Y[k] + sync;
     }
 
-    double dz1(double x1, double y1, double z1, double x2, double y2, double z2)
+    double dz(double *X, double *Y, double *Z, int k, int l)
     {
-        return 0.1 + z1 * (x1 - 8.5);
+        return 0.1 + Z[k] * (X[k] - 8.5);
     }
 
-    double dx2(double x1, double y1, double z1, double x2, double y2, double z2)
-    {
-        return -w2 * y2 - z2;
-    }
-
-    double dy2(double x1, double y1, double z1, double x2, double y2, double z2)
-    {
-        if ((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2) < r * r)
-            return w2 * x2 + a * y2 + d * (y1 - y2);
-        else return w2 * x2 + a * y2;
-    }
-
-    double dz2(double x1, double y1, double z1, double x2, double y2, double z2)
-    {
-        return 0.1 + z2 * (x2 - 8.5);
-    }
-
-    void paintGraphs();
+//    void paintGraphs();
 
 protected:
     void timerEvent(QTimerEvent *e);
